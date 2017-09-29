f9df4253354f03dd93c84caa355c1a551ad5e9de
Carlos Velasco <carlos@2activedesign.com>
2017-09-29 15:51:58 +0200

    date correction

--- sw.js
+++ sw.js
@@ -47,11 +47,11 @@ var __wpo = {
     "5a8465896222227807ff29908d8648db510561a5": "./fonts/roboto/Roboto-Bold.woff2",
     "e0655b6097eddf9df88cd194eff08436ea086a1c": "./fonts/roboto/Roboto-Bold.woff",
     "35c43198fe5d921fe627df3ea4661afa296742fa": "./src/banner-02.svg",
-    "c9df9c40e01fe4b1979dea1c4ccd367ec92c8582": "./bundle.js"
+    "ee35e22bcc667caa1809133a173f128db1e47985": "./bundle.js"
   },
   "strategy": "all",
   "responseStrategy": "network-first",
-  "version": "2017-9-29 15:46:25",
+  "version": "2017-9-29 15:51:48",
   "name": "webpack-offline",
   "pluginVersion": "4.8.4",
   "relativePaths": true
