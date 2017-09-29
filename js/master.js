6ba31791f30f53f7a23746e03bb63b74a61fe690
Carlos Velasco <carlos@2activedesign.com>
2017-09-29 16:59:58 +0200

    sizing

--- js/master.js
+++ js/master.js
@@ -197,7 +197,10 @@ window.addEventListener("load", function() {
   function error(err) {
     console.log(err);
   }
+  function nextCarousel() {
+    $(".carousel").carousel("next");
+  }
+  window.setInterval(nextCarousel, 8000);
 });
-
 // listMovies(Peliculas);
 roundListMovies(M9);
--- sw.js
+++ sw.js
@@ -47,11 +47,11 @@ var __wpo = {
     "5a8465896222227807ff29908d8648db510561a5": "./fonts/roboto/Roboto-Bold.woff2",
     "e0655b6097eddf9df88cd194eff08436ea086a1c": "./fonts/roboto/Roboto-Bold.woff",
     "35c43198fe5d921fe627df3ea4661afa296742fa": "./src/banner-02.svg",
-    "5e043532e7fea729c20cacb8bf34838d3d59dd5f": "./bundle.js"
+    "923714f6dc445529ce4dbc35dc2a7313bb890719": "./bundle.js"
   },
   "strategy": "all",
   "responseStrategy": "network-first",
-  "version": "2017-9-29 16:08:37",
+  "version": "2017-9-29 16:59:46",
   "name": "webpack-offline",
   "pluginVersion": "4.8.4",
   "relativePaths": true
